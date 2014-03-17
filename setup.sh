608d7a69a161d12c6b2ee1ffd077ab0311cdf960
atitsbest <atitsbest@gmail.com>
2014-03-17 15:38:38 +0100

    + Setup-Script für *NIX

---
+++ setup.sh
@@ -0,0 +1,20 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf $DIR/vimrc $HOME/.vimrc 
+ln -sf $DIR/agignore $HOME/.agignore 
+ln -sf $DIR/aliases $HOME/.aliases 
+ln -sf $DIR/gemrc $HOME/.gemrc 
+ln -sf $DIR/gitconfig $HOME/.gitconfig 
+ln -sf $DIR/gitignore $HOME/.gitignore 
+ln -sf $DIR/gitmessage $HOME/.gitmessage 
+ln -sf $DIR/gvimrc $HOME/.gvimrc 
+ln -sf $DIR/psqlrc $HOME/.psqlrc 
+ln -sf $DIR/tmux.conf $HOME/.tmux.conf 
+ln -sf $DIR/vimrc $HOME/.vimrc 
+ln -sf $DIR/vimrc.bundles $HOME/.vimrc.bundles 
+ln -sf $DIR/zlogin $HOME/.zlogin 
+ln -sf $DIR/zshrc $HOME/.zshrc 
+
+ln -sf $DIR/vim $HOME/.vim 
+ln -sf $DIR/zsh $HOME/.zsh 
+
